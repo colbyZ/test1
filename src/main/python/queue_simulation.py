3d9a7c0ae7abc60a83cbea5db626b7c12d1828b1
Timur Zambalayev <timur@jetbrains.com>
2016-09-04 21:13:10 -0400

    simulation()

--- src/main/python/queue_simulation.py
+++ src/main/python/queue_simulation.py
@@ -13,3 +13,37 @@ print 'arrival times: \n%s\n' % arrival_times
 
 service_times = np.random.uniform(5.0, 15.0, 100)
 print 'service times:\n%s\n' % service_times
+
+from collections import deque
+
+
+def simulation(arrival_times, service_times):
+    queue = deque()
+    wait_times = []
+
+    for i in range(len(arrival_gaps)):
+        arrival_time = arrival_times[i]
+
+        while queue and queue[0] <= arrival_time:
+            if queue[0] <= arrival_time:
+                queue.popleft()
+
+        service_time = service_times[i]
+
+        if queue:
+            service_start_time = queue[-1]
+        else:
+            service_start_time = arrival_time
+
+        service_finish_time = service_start_time + service_time
+        queue.append(service_finish_time)
+        wait_time = service_start_time - arrival_time
+        wait_times.append(wait_time)
+
+    avg_wait_time = np.mean(wait_times)
+    print avg_wait_time
+
+    return 0, avg_wait_time
+
+
+simulation(arrival_times, service_times)
