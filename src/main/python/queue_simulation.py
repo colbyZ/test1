2ce3b779132a7317ac1196c6ddca5c5014867832
Timur Zambalayev <timur@jetbrains.com>
2016-09-10 11:43:15 -0400

    hw1

--- src/main/python/queue_simulation.py
+++ src/main/python/queue_simulation.py
@@ -1,30 +1,21 @@
 import numpy as np
 
 from collections import deque
+from itertools import izip
 
 
 def simulation(arrival_times, service_times):
     queue = deque()
-    wait_times = []
-
     queue_length_by_time_sum = 0.0
-
     prev_time = 0.0
-
-    for i in range(len(arrival_times)):
-        arrival_time = arrival_times[i]
+    wait_times = []
+    for arrival_time, service_time in izip(arrival_times, service_times):
 
         while queue and queue[0] <= arrival_time:
             queue_length_by_time_sum += len(queue) * (queue[0] - prev_time)
             prev_time = queue.popleft()
 
-        service_time = service_times[i]
-
-        if queue:
-            service_start_time = queue[-1]
-        else:
-            service_start_time = arrival_time
-
+        service_start_time = queue[-1] if queue else arrival_time
         service_finish_time = service_start_time + service_time
 
         queue_length_by_time_sum += len(queue) * (arrival_time - prev_time)
@@ -58,17 +49,22 @@ def get_arrival_and_service_times():
     return arrival_times, service_times
 
 
-avg_wait_times = []
-avg_queue_lengths = []
+def main():
+    avg_wait_times = []
+    avg_queue_lengths = []
 
-for i in range(500):
-    arrival_times, service_times = get_arrival_and_service_times()
-    avg_queue_length, avg_wait_time = simulation(arrival_times, service_times)
+    for i in range(500):
+        arrival_times, service_times = get_arrival_and_service_times()
+        avg_queue_length, avg_wait_time = simulation(arrival_times, service_times)
 
-    avg_wait_times.append(avg_wait_time)
-    avg_queue_lengths.append(avg_queue_length)
+        avg_wait_times.append(avg_wait_time)
+        avg_queue_lengths.append(avg_queue_length)
 
-print 'avg wait times, mean: %.2f, std: %.2f' % (np.mean(avg_wait_times), np.std(avg_wait_times))
-print 'avg queue lengths, mean: %.2f, std: %.2f' % (np.mean(avg_queue_lengths), np.std(avg_queue_lengths))
+    print 'avg wait times, mean: %.2f, std: %.2f' % (np.mean(avg_wait_times), np.std(avg_wait_times))
+    print 'avg queue lengths, mean: %.2f, std: %.2f' % (np.mean(avg_queue_lengths), np.std(avg_queue_lengths))
 
-print avg_wait_times
+    print avg_wait_times
+
+
+if __name__ == '__main__':
+    main()
