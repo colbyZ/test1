9a82ad236d9575234e14773bb832443bc10da3d2
Timur Zambalayev <timur@jetbrains.com>
2016-09-17 12:39:35 -0400

    split()

--- hw/hw2/prob_1_a.py
+++ hw/hw2/prob_1_a.py
@@ -1,5 +1,14 @@
+import pandas as pd
+
+
+def split(data, m):
+    return data, data
+
+
 def compare_with_sklearn():
-    print 'compare with sklearn'
+    df = pd.read_csv('dataset/dataset_1_full.txt')
+    train, test = split(df, 0.7)
+    print train.shape, test.shape
 
 
 if __name__ == '__main__':
