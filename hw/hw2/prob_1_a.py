0ba643494ae58bc60e41a0527c6a567bffe6e895
Timur Zambalayev <timur@jetbrains.com>
2016-09-17 12:44:55 -0400

    split

--- hw/hw2/prob_1_a.py
+++ hw/hw2/prob_1_a.py
@@ -1,14 +1,29 @@
+import numpy as np
 import pandas as pd
 
 
 def split(data, m):
-    return data, data
+    test_size = 1 - m
+    length = len(data)
+
+    indices = list(data.index)
+    np.random.shuffle(indices)
+
+    test_length = int(round(test_size * length))
+    test_indices = indices[:test_length]
+    train_indices = indices[test_length:]
+
+    test = data.loc[test_indices]
+    train = data.loc[train_indices]
+
+    return train, test
 
 
 def compare_with_sklearn():
     df = pd.read_csv('dataset/dataset_1_full.txt')
     train, test = split(df, 0.7)
     print train.shape, test.shape
+    print train.head()
 
 
 if __name__ == '__main__':
