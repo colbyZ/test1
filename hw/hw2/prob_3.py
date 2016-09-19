21cbfc27af0bfffe7f551642f14e2b0f7b9e651d
Timur Zambalayev <timur@jetbrains.com>
2016-09-18 20:02:36 -0400

    lin_reg

--- hw/hw2/prob_3.py
+++ hw/hw2/prob_3.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression as Lin_Reg
+
+
+def reshape(df, column_name):
+    return df[column_name].reshape((len(df), 1))
 
 
 def read_dataset(dataset_i):
@@ -31,9 +36,16 @@ def residual_plots():
     ax.set_ylabel('y')
     ax.set_title('Dataset 1')
 
+    x_train = reshape(df, 'x')
+    y_train = reshape(df, 'y')
+
+    lin_reg = Lin_Reg()
+    lin_reg.fit(x_train, y_train)
+
     x = np.arange(-0.1, 2.0, step=1.2)
     plt.plot(x, 0.4 * x + 0.2, label='slope = 0.4, intercept = 0.2')
     plt.plot(x, 0.4 * x + 4, label='slope = 0.4, intercept = 4')
+    plt.plot(x, lin_reg.coef_[0][0] * x + lin_reg.intercept_[0], label='linear regression model')
     plt.legend(loc='upper left')
 
     plt.show()
