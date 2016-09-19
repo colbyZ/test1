3f30dc4317ea9efcf617bbad07aaf4442ca24680
Timur Zambalayev <timur@jetbrains.com>
2016-09-18 20:41:59 -0400

    residual_plots

--- hw/hw2/prob_3.py
+++ hw/hw2/prob_3.py
@@ -1,3 +1,5 @@
+from itertools import izip
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -23,17 +25,18 @@ def read_and_visualize_dataset():
     plt.show()
 
 
-def residual_plots():
+def visualize_fit():
     df = read_dataset(1)
 
+    x_values = df['x'].values
+    y_values = df['y'].values
+
     fig, ax = plt.subplots(1, 1, figsize=(12, 5))
-    x_values = df[['x']].values
-    y_values = df[['y']].values
 
     ax.scatter(x_values, y_values)
     ax.set_xlabel('x')
     ax.set_ylabel('y')
-    ax.set_title('Dataset 1')
+    ax.set_title('different linear fits')
 
     x_train = reshape(df, 'x')
     y_train = reshape(df, 'y')
@@ -42,14 +45,40 @@ def residual_plots():
     lin_reg.fit(x_train, y_train)
 
     x = np.arange(-0.1, 2.0, step=1.2)
-    plt.plot(x, 0.4 * x + 0.2, label='slope = 0.4, intercept = 0.2')
-    plt.plot(x, 0.4 * x + 4, label='slope = 0.4, intercept = 4')
-    plt.plot(x, lin_reg.coef_[0][0] * x + lin_reg.intercept_[0], label='linear regression model')
-    plt.legend(loc='upper left')
+    ax.plot(x, 0.4 * x + 0.2, label='slope = 0.4, intercept = 0.2')
+    ax.plot(x, 0.4 * x + 4, label='slope = 0.4, intercept = 4')
+    ax.plot(x, lin_reg.coef_[0][0] * x + lin_reg.intercept_[0], label='linear regression model')
+    ax.legend(loc='upper left')
+
+    plt.show()
+
+
+def compute_residuals(x_values, y_values, slope, intercept):
+    residuals = []
+    for x, y in izip(x_values, y_values):
+        residuals.append(y - (x * slope + intercept))
+    return residuals
+
+
+def residual_plots():
+    df = read_dataset(1)
+
+    x_values = df['x'].values
+    y_values = df['y'].values
+
+    fig, ax = plt.subplots(1, 1, figsize=(12, 5))
+
+    residuals = compute_residuals(x_values, y_values, 0.4, 0.2)
+
+    ax.scatter(x_values, residuals)
+    ax.set_xlabel('x')
+    ax.set_ylabel('residuals')
+    ax.set_title('slope = 0.4, intercept = 0.2')
 
     plt.show()
 
 
 if __name__ == '__main__':
     # read_and_visualize_dataset()
+    # visualize_fit()
     residual_plots()
