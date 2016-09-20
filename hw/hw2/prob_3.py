bd7d1d5ecf0ff066b708c2492f57896c35c06033
Timur Zambalayev <timur@jetbrains.com>
2016-09-20 19:46:49 -0400

    dataset_1_data = prepare_dataset_1_data()

--- hw/hw2/prob_3.py
+++ hw/hw2/prob_3.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from itertools import izip
 
 import matplotlib.pyplot as plt
@@ -5,13 +6,28 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression as Lin_Reg
 
+Dataset_1_Data = namedtuple('Dataset_1_Data', ['df', 'lin_reg'])
+
 
 def read_dataset(dataset_i):
     return pd.read_csv('./dataset/dataset_%d_full.txt' % dataset_i)
 
 
-def read_and_visualize_dataset():
+def reshape_column(df, column_name):
+    return df[column_name].reshape(-1, 1)
+
+
+def prepare_dataset_1_data():
     df = read_dataset(1)
+    x_train = reshape_column(df, 'x')
+    y_train = reshape_column(df, 'y')
+    lin_reg = Lin_Reg()
+    lin_reg.fit(x_train, y_train)
+    return Dataset_1_Data(df, lin_reg)
+
+
+def read_and_visualize_dataset():
+    df = dataset_1_data.df
 
     fig, ax1 = plt.subplots(1, 1, figsize=(15, 5))
     ax1.scatter(df[['x']].values, df[['y']].values)
@@ -21,15 +37,12 @@ def read_and_visualize_dataset():
     plt.show()
 
 
-def reshape_column(df, column_name):
-    return df[column_name].reshape(-1, 1)
-
-
 def visualize_fit():
-    df = read_dataset(1)
+    df = dataset_1_data.df
+    lin_reg = dataset_1_data.lin_reg
 
     x_values = df['x']
-    y_values = df['y'].values
+    y_values = df['y']
 
     fig, ax = plt.subplots(1, 1, figsize=(12, 5))
 
@@ -37,12 +50,6 @@ def visualize_fit():
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_title('different linear fits')
-
-    x_train = reshape_column(df, 'x')
-    y_train = reshape_column(df, 'y')
-
-    lin_reg = Lin_Reg()
-    lin_reg.fit(x_train, y_train)
 
     x = np.arange(-0.1, 2.0, step=1.2)
     ax.plot(x, 0.4 * x + 0.2, label='slope = 0.4, intercept = 0.2')
@@ -62,16 +69,11 @@ def compute_residuals(x_values, y_values, slope, intercept):
 
 
 def residual_plots():
-    df = read_dataset(1)
+    df = dataset_1_data.df
+    lin_reg = dataset_1_data.lin_reg
 
     x_values = df['x']
     y_values = df['y']
-
-    x_train = reshape_column(df, 'x')
-    y_train = reshape_column(df, 'y')
-
-    lin_reg = Lin_Reg()
-    lin_reg.fit(x_train, y_train)
 
     fig, axes = plt.subplots(3, 2, figsize=(12, 15))
 
@@ -98,7 +100,14 @@ def residual_plots():
     plt.show()
 
 
+def calculate_r_squared_coefs():
+    pass
+
+
 if __name__ == '__main__':
+    dataset_1_data = prepare_dataset_1_data()
+
     # read_and_visualize_dataset()
     # visualize_fit()
-    residual_plots()
+    # residual_plots()
+    calculate_r_squared_coefs()
