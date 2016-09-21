8701a89a5b46e5d782c3893c3863e4f41d322457
Timur Zambalayev <timur@jetbrains.com>
2016-09-21 18:52:44 -0400

    add_three_uniformly_distributed_variables()

--- hw/hw2/prob_4.py
+++ hw/hw2/prob_4.py
@@ -1,5 +1,3 @@
-from itertools import izip
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,16 +7,19 @@ def create_uniform_sample(n):
     return xs
 
 
-def add_two_uniformly_distributed_variables():
-    np.random.seed(1090)
-
-    n = 2000000
-    xs = create_uniform_sample(n)
-    ys = create_uniform_sample(n)
+def add_variables(variable_list):
     zs = []
-    for x, y in izip(xs, ys):
-        zs.append(x + y)
+    for i, vs in enumerate(variable_list):
+        if i == 0:
+            for v in vs:
+                zs.append(v)
+        else:
+            for j, v in enumerate(vs):
+                zs[j] += v
+    return zs
 
+
+def show_histogram(zs):
     fig, ax = plt.subplots(1, 1, figsize=(12, 5))
 
     ax.hist(zs, 400, normed=True)
@@ -27,5 +28,30 @@ def add_two_uniformly_distributed_variables():
     plt.show()
 
 
+def add_two_uniformly_distributed_variables():
+    np.random.seed(1090)
+
+    n = 2000000
+    xs = create_uniform_sample(n)
+    ys = create_uniform_sample(n)
+    zs = add_variables([xs, ys])
+
+    show_histogram(zs)
+
+
+def add_three_uniformly_distributed_variables():
+    np.random.seed(1090)
+
+    n = 2000000
+    zs = add_variables([
+        (create_uniform_sample(n)),
+        (create_uniform_sample(n)),
+        (create_uniform_sample(n)),
+    ])
+
+    show_histogram(zs)
+
+
 if __name__ == '__main__':
-    add_two_uniformly_distributed_variables()
+    # add_two_uniformly_distributed_variables()
+    add_three_uniformly_distributed_variables()
