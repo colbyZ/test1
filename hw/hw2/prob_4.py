df4608f9541610afc15ca543b12ef9e8314303ec
Timur Zambalayev <timur@jetbrains.com>
2016-09-21 20:29:07 -0400

    add_normal_variables(2)

--- hw/hw2/prob_4.py
+++ hw/hw2/prob_4.py
@@ -5,40 +5,48 @@ import numpy as np
 from scipy.stats import norm
 
 
-def create_uniform_sample(n):
-    return np.random.uniform(0.0, 1.0, n)
-
-
 def add_variables(variable_list):
     return (sum(elements) for elements in izip(*variable_list))
 
 
-def show_histogram(zs, k):
+def show_histogram(zs, mean, std):
     fig, ax = plt.subplots(1, 1, figsize=(12, 5))
 
     ax.hist(list(zs), 400, normed=True)
 
     # plot the pdf of the normal distribution
-    xs = np.linspace(0, k, num=100)
-    ax.plot(xs, norm.pdf(xs, k / 2.0, np.sqrt(k / 12.0)))
+    xs = np.linspace(mean - 4 * std, mean + 4 * std, num=100)
+    ax.plot(xs, norm.pdf(xs, mean, std))
 
     plt.tight_layout()
     plt.show()
 
 
 def create_k_uniform_variables(n, k):
-    return (create_uniform_sample(n) for _ in range(k))
+    return (np.random.uniform(0.0, 1.0, n) for _ in range(k))
+
+
+def create_k_normal_variables(n, k):
+    return (np.random.normal(0.0, 1.0, n) for _ in range(k))
 
 
 def add_uniform_variables(k):
     sample_size = 10000000 / k
     variable_list = create_k_uniform_variables(sample_size, k)
     zs = add_variables(variable_list)
-    show_histogram(zs, k)
+    show_histogram(zs, k / 2.0, np.sqrt(k / 12.0))
+
+
+def add_normal_variables(k):
+    sample_size = 10000000 / k
+    variable_list = create_k_normal_variables(sample_size, k)
+    zs = add_variables(variable_list)
+    show_histogram(zs, 0, np.sqrt(2))
 
 
 if __name__ == '__main__':
     np.random.seed(1090)
 
     # add_uniform_variables(2)
-    add_uniform_variables(7)
+    # add_uniform_variables(7)
+    add_normal_variables(2)
