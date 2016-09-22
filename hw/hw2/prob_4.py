6df9eb95284daeb8341c6586c41bc3535c3588e3
Timur Zambalayev <timur@jetbrains.com>
2016-09-21 20:09:48 -0400

    plot the pdf of the normal distribution

--- hw/hw2/prob_4.py
+++ hw/hw2/prob_4.py
@@ -2,6 +2,7 @@ from itertools import izip
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import norm
 
 
 def create_uniform_sample(n):
@@ -12,10 +13,14 @@ def add_variables(variable_list):
     return (sum(elements) for elements in izip(*variable_list))
 
 
-def show_histogram(zs):
+def show_histogram(zs, k):
     fig, ax = plt.subplots(1, 1, figsize=(12, 5))
 
     ax.hist(list(zs), 400, normed=True)
+
+    # plot the pdf of the normal distribution
+    xs = np.linspace(0, k, num=100)
+    ax.plot(xs, norm.pdf(xs, k / 2.0, np.sqrt(k / 12.0)))
 
     plt.tight_layout()
     plt.show()
@@ -26,14 +31,14 @@ def create_k_uniform_variables(n, k):
 
 
 def add_uniform_variables(k):
-    n = 10000000 / k
-    variable_list = create_k_uniform_variables(n, k)
+    sample_size = 10000000 / k
+    variable_list = create_k_uniform_variables(sample_size, k)
     zs = add_variables(variable_list)
-    show_histogram(zs)
+    show_histogram(zs, k)
 
 
 if __name__ == '__main__':
     np.random.seed(1090)
 
     # add_uniform_variables(2)
-    add_uniform_variables(10)
+    add_uniform_variables(7)
