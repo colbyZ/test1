9ce901eeba24281a8b3595a154f08880d0155998
Timur Zambalayev <timur@jetbrains.com>
2016-09-24 13:56:10 -0400

    polynomial_regression_fit, init

--- hw/hw3/hw3_prob2.py
+++ hw/hw3/hw3_prob2.py
@@ -1,5 +1,18 @@
-def fit_polynomial_regression_models_prob_2b():
+import numpy as np
+
+
+def loadtxt(file_name):
+    return np.loadtxt('datasets/%s' % file_name, delimiter=',', skiprows=1)
+
+
+def polynomial_regression_fit(x_train, y_train, degree_of_the_polynomial):
     pass
+
+
+def fit_polynomial_regression_models_prob_2b():
+    data = loadtxt("dataset_3.txt")
+
+    print data
 
 
 if __name__ == '__main__':
