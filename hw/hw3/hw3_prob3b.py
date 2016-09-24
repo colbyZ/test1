1a9f214ad545a03b2e464a52284f7be686ff3a15
Timur Zambalayev <timur@jetbrains.com>
2016-09-24 19:43:38 -0400

    time_of_the_day_list

--- hw/hw3/hw3_prob3b.py
+++ hw/hw3/hw3_prob3b.py
@@ -1,11 +1,21 @@
+from collections import Counter
+
 import pandas as pd
 
 
 def taxicab_density_estimation():
-    nrows = 10
+    nrows = 100 * 1000
     df = pd.read_csv('green_tripdata_2015-01.csv', header=0, index_col=False, usecols=['lpep_pickup_datetime'],
                      parse_dates=['lpep_pickup_datetime'], nrows=nrows)
-    print df.ix[0]
+    day_minute_list = []
+    for index, row in df.iterrows():
+        datetime = row[0]
+        minute = datetime.minute
+        hour = datetime.hour
+        day_minute = minute + 60 * hour
+        day_minute_list.append(day_minute)
+    counter = Counter(day_minute_list)
+    time_of_the_day_list, num_pickups_list = zip(*counter.items())
 
 
 if __name__ == '__main__':
