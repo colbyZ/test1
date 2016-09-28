e1df92f107aef5f34d1bc310b344faf66c1b3639
Timur Zambalayev <timur@jetbrains.com>
2016-09-27 20:49:44 -0400

    prob_4a, init

--- hw/hw3/hw3_prob4.py
+++ hw/hw3/hw3_prob4.py
@@ -1,6 +1,20 @@
-def f():
-    pass
+import numpy as np
+
+
+def loadtxt(file_name):
+    return np.loadtxt('datasets/%s' % file_name, delimiter=',', skiprows=1)
+
+
+def prob_4a():
+    data_train = loadtxt('dataset_1_train.txt')
+    data_test = loadtxt('dataset_1_test.txt')
+
+    print data_train.shape, data_test.shape
+
+
+def main():
+    prob_4a()
 
 
 if __name__ == '__main__':
-    f()
+    main()
