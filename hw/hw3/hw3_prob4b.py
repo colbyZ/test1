fd05f9ac0a7a76dae1ba3f87b51d0819306b0b3a
Timur Zambalayev <timur@jetbrains.com>
2016-09-28 17:27:50 -0400

    weights

--- hw/hw3/hw3_prob4b.py
+++ hw/hw3/hw3_prob4b.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from sklearn.preprocessing import LabelEncoder
+
 
 def loadtxt(file_name, dtype=float):
     return np.loadtxt('datasets/%s' % file_name, delimiter=',', skiprows=1, dtype=dtype)
@@ -12,8 +14,11 @@ def weighted_linear_regression_prob_4b():
     data_test = loadtxt('dataset_1_test.txt')
     data_train_noise_levels = loadtxt('dataset_1_train_noise_levels.txt', dtype=str)
 
+    weights = [1.0 if noise_level_str == 'none' else 0.5 for noise_level_str in data_train_noise_levels]
+
+    print weights
+
     print data_train.shape, data_test.shape, data_train_noise_levels.shape
-    print data_train_noise_levels[:10]
 
 
 def main():
