29f3600d8f5b4c8b6e337ea814002944128c57a8
Timur Zambalayev <timur@jetbrains.com>
2016-09-29 22:15:47 -0400

    load_dataset_1

--- hw/hw4/hw4_prob_1.py
+++ hw/hw4/hw4_prob_1.py
@@ -1,13 +1,24 @@
+from collections import namedtuple
+
 import matplotlib.pyplot as plt
 import numpy as np
 
+Dataset_1_Data = namedtuple('Dataset_1_Data', ['x', 'y'])
 
-def prob_1a():
+
+def load_dataset_1():
     # Load data
     data = np.loadtxt('datasets/dataset_1.txt', delimiter=',', skiprows=1)
 
     # Split predictors and response
     x = data[:, :-1]
+    y = data[:, -1]
+
+    return Dataset_1_Data(x, y)
+
+
+def heatmap_prob_1a(dataset_1_data):
+    x = dataset_1_data.x
 
     # Compute matrix of correlation coefficients
     corr_matrix = np.corrcoef(x.T)
@@ -22,12 +33,19 @@ def prob_1a():
 
 
 def exhaustive_search_prob_1b():
-    pass
+    # Best Subset Selection
+    min_bic = float('inf')  # set some initial large value for min BIC score
+    best_subset = []  # best subset of predictors
+
+    # Create all possible subsets of the set of 10 predictors
+    predictor_set = range(10)  # predictor set = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 def main():
-    # prob_1a()
-    exhaustive_search_prob_1b()
+    dataset_1_data = load_dataset_1()
+
+    heatmap_prob_1a(dataset_1_data)
+    # exhaustive_search_prob_1b()
 
 
 if __name__ == '__main__':
