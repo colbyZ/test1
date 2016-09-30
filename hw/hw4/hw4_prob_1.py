c576affee3cde8676d5ae15e9aa31de794dfff79
Timur Zambalayev <timur@jetbrains.com>
2016-09-29 21:32:30 -0400

    prob_1a

--- hw/hw4/hw4_prob_1.py
+++ hw/hw4/hw4_prob_1.py
@@ -1,9 +1,21 @@
-def prob_1():
-    pass
+import numpy as np
+import pandas as pd
+
+
+def prob_1a():
+    # Load data
+    data = np.loadtxt('datasets/dataset_1.txt', delimiter=',', skiprows=1)
+
+    # Split predictors and response
+    x = data[:, :-1]
+    y = data[:, -1]
+
+    df = pd.DataFrame(data)
+    print repr(df.head())
 
 
 def main():
-    prob_1()
+    prob_1a()
 
 
 if __name__ == '__main__':
