d6a5d1507cb39d235032510668923250e528840b
Timur Zambalayev <timur@jetbrains.com>
2016-10-01 17:20:48 -0400

    split

--- hw/hw4/hw4_prob_2.py
+++ hw/hw4/hw4_prob_2.py
@@ -1,7 +1,11 @@
+from collections import namedtuple
+
 import pandas as pd
 
-
 # prob 2a
+
+Dataset_2_Data = namedtuple('Dataset_2_Data', ['x', 'y'])
+
 
 def is_categorical(column):
     return column.dtype == object or len(column.unique()) < 8
@@ -10,40 +14,53 @@ def is_categorical(column):
 def encode_categorical_variables_prob_2a():
     df = pd.read_csv('datasets/dataset_2.txt')
 
-    expanded_df = df.copy()
+    y = df['price']
+    x_df = df.drop('price', axis=1)
+
+    expanded_x_df = x_df.copy()
     for column_name in df.columns:
         column = df[column_name]
         if is_categorical(column):
             dummies_df = pd.get_dummies(column, prefix=column_name)
 
-            expanded_df = expanded_df.drop(column_name, axis=1)
-            expanded_df = pd.concat([expanded_df, dummies_df], axis=1)
+            expanded_x_df = expanded_x_df.drop(column_name, axis=1)
+            expanded_x_df = pd.concat([expanded_x_df, dummies_df], axis=1)
 
-    return expanded_df
+    return Dataset_2_Data(expanded_x_df, y)
 
 
-def print_expanded_df_prob_2a(df):
-    print '%d columns:' % len(df.columns)
-    print df.columns
+def print_expanded_df_prob_2a(dataset_2_data):
+    x = dataset_2_data.x
+
+    print '%d columns:' % len(x.columns)
+    print x.columns
 
 
 # prob 2b
 
-def linear_regression_prob_2b(df):
-    split_index = len(df) // 4
-
+def split(df, split_index):
     train = df[:split_index]
     test = df[split_index:]
+    return train, test
 
-    print len(train), len(test)
-    print train.head()
+
+def linear_regression_prob_2b(dataset_2_data):
+    x = dataset_2_data.x
+    y = dataset_2_data.y
+
+    split_index = len(x) // 4
+
+    x_train, x_test = split(x, split_index)
+    y_train, y_test = split(y, split_index)
+
+    print x_train.shape, x_test.shape
 
 
 def main():
-    expanded_df = encode_categorical_variables_prob_2a()
+    dataset_2_data = encode_categorical_variables_prob_2a()
 
-    # print_expanded_df_prob_2a(expanded_df)
-    linear_regression_prob_2b(expanded_df)
+    # print_expanded_df_prob_2a(dataset_2_data)
+    linear_regression_prob_2b(dataset_2_data)
 
 
 if __name__ == '__main__':
