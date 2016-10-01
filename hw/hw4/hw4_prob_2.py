feb55034d163a03667fda236e9aea8f6afbb887a
Timur Zambalayev <timur@jetbrains.com>
2016-10-01 16:47:15 -0400

    is_categorical()

--- hw/hw4/hw4_prob_2.py
+++ hw/hw4/hw4_prob_2.py
@@ -1,30 +1,24 @@
 import pandas as pd
 
 
-def has_limited_unique_values(column):
-    unique_values = set(column)
-    num_unique_values = len(unique_values)
-    return num_unique_values <= 8
-
-
 def is_categorical(column):
-    return column.dtype == object or has_limited_unique_values(column)
+    return column.dtype == object or len(column.unique()) < 8
 
 
 def encode_categorical_variables_prob_2a():
     df = pd.read_csv('datasets/dataset_2.txt')
 
-    dummy_df = df.copy()
+    expanded_df = df.copy()
     for column_name in df.columns:
         column = df[column_name]
         if is_categorical(column):
             dummies_df = pd.get_dummies(column, prefix=column_name)
 
-            dummy_df = dummy_df.drop(column_name, axis=1)
-            dummy_df = pd.concat([dummy_df, dummies_df], axis=1)
+            expanded_df = expanded_df.drop(column_name, axis=1)
+            expanded_df = pd.concat([expanded_df, dummies_df], axis=1)
 
-    print dummy_df.columns
-    print len(dummy_df.columns)
+    print expanded_df.columns
+    print len(expanded_df.columns)
 
 
 def main():
