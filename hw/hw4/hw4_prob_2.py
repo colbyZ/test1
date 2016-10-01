2e984db743531656724a4315252ae1456f428df1
Timur Zambalayev <timur@jetbrains.com>
2016-10-01 16:36:33 -0400

    encode_categorical_variables_prob_2a

--- hw/hw4/hw4_prob_2.py
+++ hw/hw4/hw4_prob_2.py
@@ -1,9 +1,30 @@
 import pandas as pd
 
 
+def has_limited_unique_values(column):
+    unique_values = set(column)
+    num_unique_values = len(unique_values)
+    return num_unique_values <= 8
+
+
+def is_categorical(column):
+    return column.dtype == object or has_limited_unique_values(column)
+
+
 def encode_categorical_variables_prob_2a():
     df = pd.read_csv('datasets/dataset_2.txt')
-    print df.columns
+
+    dummy_df = df.copy()
+    for column_name in df.columns:
+        column = df[column_name]
+        if is_categorical(column):
+            dummies_df = pd.get_dummies(column, prefix=column_name)
+
+            dummy_df = dummy_df.drop(column_name, axis=1)
+            dummy_df = pd.concat([dummy_df, dummies_df], axis=1)
+
+    print dummy_df.columns
+    print len(dummy_df.columns)
 
 
 def main():
