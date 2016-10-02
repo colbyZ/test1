3a82155a6cb671eb1fb67d16513899d339b25d05
Timur Zambalayev <timur@jetbrains.com>
2016-10-02 19:53:55 -0400

    scaler

--- hw/hw4/hw4_prob_3b.py
+++ hw/hw4/hw4_prob_3b.py
@@ -1,6 +1,11 @@
+from collections import namedtuple
+
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+
+RidgeRegressionModel = namedtuple('RidgeRegressionModel', ['linear_regression', 'x_scaler'])
 
 
 def split(data, split_index):
@@ -11,28 +16,35 @@ def split(data, split_index):
 
 # Fit
 
-def get_aug_x_y(reg_param, x_train, y_train):
-    n = len(x_train)
-    num_predictors = x_train.shape[1]
-    aug_x_train = np.concatenate((x_train, np.sqrt(reg_param) * np.identity(n)))
-    aug_y_train = np.concatenate((y_train, np.zeros(num_predictors))).reshape(-1, 1)
-    return aug_x_train, aug_y_train
+def get_aug_x_y(reg_param, x, y):
+    n = len(x)
+    num_predictors = x.shape[1]
+    aug_x = np.concatenate((x, np.sqrt(reg_param) * np.identity(n)))
+    aug_y = np.concatenate((y, np.zeros(num_predictors))).reshape(-1, 1)
+    return aug_x, aug_y
 
 
 def get_ridge_regression(x_train, y_train, reg_param):
-    aug_x_train, aug_y_train = get_aug_x_y(reg_param, x_train, y_train)
+    x_scaler = StandardScaler(with_mean=False)
+    x_scaler.fit(x_train)
 
-    model = LinearRegression()
-    model.fit(aug_x_train, aug_y_train)
+    scaled_x_train = x_scaler.transform(x_train)
 
-    return model
+    aug_x_train, aug_y_train = get_aug_x_y(reg_param, scaled_x_train, y_train)
+
+    linear_regression = LinearRegression()
+    linear_regression.fit(aug_x_train, aug_y_train)
+
+    return RidgeRegressionModel(linear_regression, x_scaler)
 
 
 # Score
 
 def score(model, x, y, reg_param):
-    aug_x, aug_y = get_aug_x_y(reg_param, x, y)
-    return model.score(aug_x, aug_y)
+    scaled_x = model.x_scaler.transform(x)
+
+    aug_x, aug_y = get_aug_x_y(reg_param, scaled_x, y)
+    return model.linear_regression.score(aug_x, aug_y)
 
 
 def ridge_regression_prob_3b():
@@ -71,8 +83,9 @@ def ridge_regression_prob_3b():
     plt.xlabel('$\lambda$')
     plt.ylabel('$R^2$')
     plt.title('train and test R^2 scores as a function of the regularization parameter')
+    plt.ylim([-5.0, 1.3])
 
-    plt.legend()
+    plt.legend(loc='lower right')
 
     plt.show()
 
