4df6d00de7f4e3035469f58de7523549987fe62a
Timur Zambalayev <timur@jetbrains.com>
2016-10-02 21:32:10 -0400

    x, y split

--- hw/hw4/hw4_prob_4.py
+++ hw/hw4/hw4_prob_4.py
@@ -1,5 +1,14 @@
+import pandas as pd
+
+
 def fit_regression_model_prob_4a():
-    pass
+    df = pd.read_csv('datasets/dataset_4.txt', dtype={'NOEXCH': object})
+
+    y_column_name = 'TARGET_D'
+    y = df[y_column_name]
+    x_df = df.drop(y_column_name, axis=1)
+
+    print x_df.shape, y.shape
 
 
 def main():
