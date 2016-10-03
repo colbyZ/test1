b1cdde203fff04f670e949cfda50e447c99afa9f
Timur Zambalayev <timur@jetbrains.com>
2016-10-02 22:55:50 -0400

    non zero coefs

--- hw/hw4/hw4_prob_4.py
+++ hw/hw4/hw4_prob_4.py
@@ -32,16 +32,15 @@ def fit_regression_model_prob_4a():
 
     alphas = [10.0 ** i for i in np.arange(-4.0, -1.1, step=0.1)]
 
-    tolerance = 2e-3
     for alpha in alphas:
-        regression = Lasso_Reg(alpha=alpha, normalize=True, tol=tolerance)
+        regression = Lasso_Reg(alpha=alpha, normalize=True, tol=2e-3)
         regression.fit(x_train, y_train)
         train_score = regression.score(x_train, y_train)
         test_score = regression.score(x_test, y_test)
 
-        num_non_zero_coefs = sum(abs(coef) > tolerance for coef in regression.coef_)
+        num_non_zero_coefs = sum(abs(coef) > 0.0 for coef in regression.coef_)
 
-        print 'alpha: %.2e, test R^2: %.3f, train R^2: %.3f, num_non_zero_coefs: %d' % (
+        print 'alpha: %.2e, test R^2: %.3f, train R^2: %.3f, num_non_zero_coefs: %4d' % (
             alpha, test_score, train_score, num_non_zero_coefs)
 
 
