a1b06fe1e01154431d09bb12a6672d5e4f16cdb6
Timur Zambalayev <timur@jetbrains.com>
2016-10-05 11:46:56 -0400

    fixes

--- hw/hw4/hw4_common.py
+++ hw/hw4/hw4_common.py
@@ -11,14 +11,14 @@ def is_categorical(column):
     return column.dtype == object or len(column.unique()) < 8
 
 
-def convert_categorical_columns(x_df):
-    expanded_x_df = x_df.copy()
-    for column_name in x_df.columns:
-        column = x_df[column_name]
+def convert_categorical_columns(df):
+    expanded_df = df.copy()
+    for column_name in df.columns:
+        column = df[column_name]
         if is_categorical(column):
             dummies_df = pd.get_dummies(column, prefix=column_name)
 
-            expanded_x_df.drop(column_name, axis=1, inplace=True)
-            expanded_x_df = expanded_x_df.join(dummies_df)
+            expanded_df.drop(column_name, axis=1, inplace=True)
+            expanded_df = expanded_df.join(dummies_df)
 
-    return expanded_x_df
+    return expanded_df
