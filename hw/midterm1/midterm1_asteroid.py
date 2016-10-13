e7ba8d9bb60894c1410ac503435cbbef6b7a181b
Timur Zambalayev <timur@jetbrains.com>
2016-10-13 16:46:51 -0400

    show_scatter_plots(df)

--- hw/midterm1/midterm1_asteroid.py
+++ hw/midterm1/midterm1_asteroid.py
@@ -1,6 +1,11 @@
+from collections import namedtuple
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
+
+ColumnInfo = namedtuple('ColumnInfo', ['values', 'name', 'units'])
 
 
 def get_row(row_tag, cell_tag_name):
@@ -25,10 +30,50 @@ def get_data_frame():
     return pd.DataFrame(data_array, columns=header_values)
 
 
+def get_label_text(column):
+    return '%s (%s)' % (column.name, column.units)
+
+
+def show_scatter_plot(ax, x_column, y_column):
+    ax.scatter(x_column.values, y_column.values)
+
+    ax.set_xlabel(get_label_text(x_column))
+    ax.set_ylabel(get_label_text(y_column))
+
+    ax.set_title('%s vs %s (scatter plot)' % (x_column.name, y_column.name))
+
+
+def show_scatter_plots(df):
+    times = df['Time']
+    xs = df['X-Coord']
+    ys = df['Y-Coord']
+    zs = df['Z-Coord']
+
+    x_column = ColumnInfo(xs, 'x', 'km')
+    y_column = ColumnInfo(ys, 'y', 'km')
+    z_column = ColumnInfo(zs, 'z', 'km')
+    time_column = ColumnInfo(times, 'time', 'secs')
+
+    fig, axes = plt.subplots(2, 3, figsize=(20, 10))
+
+    show_scatter_plot(axes[0][0], x_column, z_column)
+    show_scatter_plot(axes[0][1], y_column, z_column)
+    show_scatter_plot(axes[0][2], time_column, z_column)
+
+    show_scatter_plot(axes[1][0], x_column, y_column)
+    show_scatter_plot(axes[1][1], time_column, y_column)
+    show_scatter_plot(axes[1][2], x_column, time_column)
+
+    plt.tight_layout()
+    plt.show()
+
+
 def asteroid():
     df = get_data_frame()
 
     print 'dataframe:\n%s' % df
+
+    show_scatter_plots(df)
 
 
 def main():
